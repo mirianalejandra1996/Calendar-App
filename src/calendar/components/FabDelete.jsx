ec130b834722fc62cb226c17e7c594913c2cf55c
mirian alejandra <mirianalejandra1996@gmail.com>
2023-08-08 16:46:41 -0500

    add project

---
+++ src/calendar/components/FabDelete.jsx
@@ -0,0 +1,34 @@
+import { useAuthStore, useCalendarStore, useUiStore } from "../../hooks"
+
+export const FabDelete = () => {
+
+    const { startDeletingEvent, activeEvent, hasEventSelected } = useCalendarStore()
+    const { isDateModalOpen } = useUiStore()
+    const { user } = useAuthStore()
+    const isMyEvent = user?.uid === activeEvent?.user?._id
+
+    const handleDelete = () => {
+
+        if(!activeEvent) return
+
+        startDeletingEvent(activeEvent)
+      }
+
+  return (
+    <>
+    {
+      isMyEvent
+      ? (
+        <button
+        onClick={handleDelete}
+        className='btn btn-danger fab-danger'
+        style={{display: (hasEventSelected && !isDateModalOpen ) ? '' : 'none'}}
+    >
+        <i className='fas fa-trash-alt'></i>
+    </button>
+      )
+      : null
+    }
+    </>
+  )
+}
---
+++ src/helpers/getEnvVariables.js
@@ -0,0 +1,8 @@
+
+export const getEnvVariables = () => {
+ 
+    return {
+        ...process.env
+    }
+
+}
