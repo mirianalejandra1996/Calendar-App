af2869989bb3a0c3a554750d13075dfc5b453422
alejandra arevalo <mirianalejandra@gmail.com>
2023-12-09 00:25:38 -0500

    add project to vite directory

--- src/helpers/getEnvVariables.js
+++
@@ -1,8 +0,0 @@
-
-export const getEnvVariables = () => {
- 
-    return {
-        ...process.env
-    }
-
-}
--- src/shared/pages/LoadingPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Ripples } from "@uiball/loaders";
-
-export const LoadingPage = () => {
-  return (
-    <div className="fullscreen-spinner">
-      <Ripples size={120} speed={2} color="#fff" />
-    </div>
-  )
-}
